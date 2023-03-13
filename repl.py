ca221e83b9277b1dbd0a7b6c2678476efb020efd
Rishav-hub <9930046@gmail.com>
2023-03-13 17:18:23 +0530

    Updated the output to 4 samples

--- repl.py
+++ repl.py
@@ -10,7 +10,8 @@ def fetch_replicate(imageUrl, room, theme):
             "image": open(imageUrl, 'rb'),
             "prompt": f"a {'room for gaming' if room == 'Gaming Room' else theme.lower()} {room.lower()}",
             "a_prompt": "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning, dubai style",
-            "n_prompt": "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality"
+            "n_prompt": "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
+            "num_samples": "4"
         }
 
 
@@ -19,8 +20,8 @@ def fetch_replicate(imageUrl, room, theme):
     return output
 
 if __name__ == '__main__':
-    output = fetch_replicate('image\my_image.jpg', 'Dining Room', 'Modern')
+    output = fetch_replicate('image\my_image.jpg', 'Dining Room', 'Ancient')
     json_response = {
-        'image_url' : output[1]
+        'image_url' : output[1:]
     }
     print(json_response)
